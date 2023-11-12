b0c72665fdf88fc7b686fefbbac2982902a783fb
ohtak6843_Desktop <ohtak6843@gmail.com>
2023-11-12 22:45:51 +0900

    heart class 수정

--- TermProject/heart.py
+++ TermProject/heart.py
@@ -8,12 +8,14 @@ class Heart:
     x = 66
     y = 700
 
-    def __init__(self):
+    def __init__(self, HP = 6):
+        self.HP = HP
+
         if Heart.image == None:
             Heart.image = load_image('Heart.png')
 
-    def draw(self, HP):
-        for i in range(0, HP):
+    def draw(self):
+        for i in range(0, self.HP):
             Heart.image.draw(self.x + self.sizeX * i, self.y, self.sizeX, self.sizeY)
 
     def update(self):
