9677ff27072eaa06885c21f7e79201ffbd5e244d
ohtak6843_Desktop <ohtak6843@gmail.com>
2023-11-12 23:09:49 +0900

    공과 벽 충돌 구현 완

--- TermProject/ball.py
+++ TermProject/ball.py
@@ -11,12 +11,34 @@ class Ball:
         self.image_x = image_x
         self.image_y = image_y
 
+        self.velo_x = 0
+        self.velo_y = 0
+
         if Ball.image == None:
             Ball.image = load_image('balls.png')
 
     def draw(self):
         Ball.image.clip_draw(self.image_x * 375, self.image_y * 375, 375, 375, self.x, self.y, 40, 40)  # 1/10 사이즈
-        # draw_rectangle(self.x - 10, self.y - 10, self.x + 10, self.y + 10)
+        # draw_rectangle(*self.get_bb())
 
     def update(self):
+        self.x -= self.velo_x
+        self.y -= self.velo_y
+        pass
+
+
+    def get_bb(self):
+        return self.x - 15, self.y - 15, self.x + 15, self.y + 15
+        pass
+
+
+    def handle_collision(self, group, other):
+        if group == 'ball:ball':
+            pass
+        elif group == 'stick:ball':
+            pass
+        elif group == 'LR_wall:ball':
+            self.velo_x = -self.velo_x
+        elif group == 'TB_wall:ball':
+            self.velo_y = -self.velo_y
         pass
