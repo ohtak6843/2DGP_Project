3f50e69cea9b9989b72727787b3fe350d155e9d3
ohtak6843_Desktop <ohtak6843@gmail.com>
2023-11-12 18:55:44 +0900

    game_framework 추가

--- TermProject/game_framework.py
+++ TermProject/game_framework.py
@@ -1,13 +1,9 @@
-from pico2d import delay
+# fill here
 import time
-
-running = None
-stack = None
-
 
 def change_mode(mode):
     global stack
-    if len(stack) > 0:
+    if (len(stack) > 0):
         # execute the current mode's finish function
         stack[-1].finish()
         # remove the current mode
@@ -18,7 +14,7 @@ def change_mode(mode):
 
 def push_mode(mode):
     global stack
-    if len(stack) > 0:
+    if (len(stack) > 0):
         stack[-1].pause()
     stack.append(mode)
     mode.init()
@@ -26,14 +22,14 @@ def push_mode(mode):
 
 def pop_mode():
     global stack
-    if len(stack) > 0:
+    if (len(stack) > 0):
         # execute the current mode's finish function
         stack[-1].finish()
         # remove the current mode
         stack.pop()
 
     # execute resume function of the previous mode
-    if len(stack) > 0:
+    if (len(stack) > 0):
         stack[-1].resume()
 
 
@@ -48,21 +44,21 @@ def run(start_mode):
     stack = [start_mode]
     start_mode.init()
 
-
+    # fill here
     global frame_time
     frame_time = 0.0
     current_time = time.time()
-
     while running:
         stack[-1].handle_events()
         stack[-1].update()
         stack[-1].draw()
-
+        # fill here
         frame_time = time.time() - current_time
-        frame_rate = 1.0 / frame_time
+        frame_rate = 1.0/frame_time
         current_time += frame_time
+        print(frame_time, frame_rate)
 
     # repeatedly delete the top of the stack
-    while len(stack) > 0:
+    while (len(stack) > 0):
         stack[-1].finish()
         stack.pop()
