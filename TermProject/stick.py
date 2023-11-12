03af7643dbd17d2d148df6f79ce62cb6e2fcac1f
ohtak6843_Desktop <ohtak6843@gmail.com>
2023-11-12 22:45:01 +0900

    큐대가 마우스 위치에 따라 움직이도록 변경

--- TermProject/stick.py
+++ TermProject/stick.py
@@ -1,15 +1,28 @@
 from pico2d import *
-
+from math import *
 
 class Stick:
     image = None
 
-    def __init__(self):
+    def __init__(self, center_x, center_y):
+        self.r = 135
+        self.center_x = center_x
+        self.center_y = center_y
+        self.mouse_x = 0
+        self.mouse_y = 0
+
         if Stick.image == None:
-            Stick.image = load_image('Heart.png')
+            Stick.image = load_image('Stick.png')
 
     def draw(self):
-        Stick.image.draw(500, 500, 240, 240)  # 1/5 사이즈
+        dy = self.center_y - self.mouse_y
+        dx = self.center_x - self.mouse_x
+        self.degree = atan2(dy, dx)
+        Stick.image.composite_draw(self.degree, '', self.center_x + self.r * cos(self.degree), self.center_y + self.r * sin(self.degree), 240, 240)  # 1/5 사이즈
+        pass
 
     def update(self):
+        pass
+
+    def handle_event(self, e):
         pass
