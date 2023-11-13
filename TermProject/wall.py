c7dc152b2f5c2016e45c948aa11b50c60019f5c7
ohtak6843_Laptop <ohtak6843@gmail.com>
2023-11-13 15:38:51 +0900

    wall class 만들기

---
+++ TermProject/wall.py
@@ -0,0 +1,20 @@
+from pico2d import *
+
+
+class Wall:
+
+    def __init__(self, left, bottom, right, top):
+        self.left = left
+        self.bottom = bottom
+        self.right = right
+        self.top = top
+
+    def draw(self):
+        draw_rectangle(*self.get_bb())
+        pass
+
+    def update(self):
+        pass
+
+    def get_bb(self):
+        return self.left, self.bottom, self.right, self.top
