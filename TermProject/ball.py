024690bd284e64e1c7908066fb728b10867b57eb
ohtak6843_Laptop <ohtak6843@gmail.com>
2023-11-20 15:19:04 +0900

    balls 이미지 조정

--- TermProject/ball.py
+++ TermProject/ball.py
@@ -19,8 +19,8 @@ class Ball:
             Ball.image = load_image('balls.png')
 
     def draw(self):
-        Ball.image.clip_draw(self.image_x * 375, self.image_y * 375, 375, 375, self.x, self.y, 40, 40)  # 1/10 사이즈
-        # draw_rectangle(*self.get_bb())
+        Ball.image.clip_draw(self.image_x * 276, self.image_y * 276, 276, 276, self.x, self.y, 40, 40)  # 1/10 사이즈
+        draw_rectangle(*self.get_bb())
 
     def update(self):
         self.x += self.velo * cos(self.degree)
@@ -29,7 +29,7 @@ class Ball:
 
 
     def get_bb(self):
-        return self.x - 15, self.y - 15, self.x + 15, self.y + 15
+        return self.x - 20, self.y - 20, self.x + 20, self.y + 20
         pass
 
 
@@ -39,6 +39,7 @@ class Ball:
         elif group == 'stick:ball':
             pass
         elif group == 'LR_wall:ball':
+            self.degree += math.pi
             pass
         elif group == 'TB_wall:ball':
             pass
