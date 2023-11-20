42f0a0a40452facff74d90d6c31b0060b6b21f0a
ohtak6843_Laptop <ohtak6843@gmail.com>
2023-11-20 15:20:00 +0900

    벽 생성 및 위치 조정

--- TermProject/main.py
+++ TermProject/main.py
@@ -1,9 +1,10 @@
 from pico2d import *
 import game_framework
+import define
 
 import play_mode as start_mode
 
 
-open_canvas(start_mode.WINDOW_WIDTH, start_mode.WINDOW_HEIGHT, sync=True)
+open_canvas(define.WINDOW_WIDTH, define.WINDOW_HEIGHT, sync=True)
 game_framework.run(start_mode)
 close_canvas()
