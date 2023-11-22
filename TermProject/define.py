d5c5f1d6f8d8c4edebac410afe3efecb332828a3
ohtak6843_Laptop <ohtak6843@gmail.com>
2023-11-22 16:39:35 +0900

    벡터 정규화 함수 추가

--- TermProject/define.py
+++ TermProject/define.py
@@ -1,1 +1,11 @@
+from math import sqrt
+
 WINDOW_WIDTH, WINDOW_HEIGHT = 1600, 900
+
+
+def normalize(vec_x, vec_y):
+    size = sqrt(vec_x ** 2 + vec_y ** 2)
+    normal_x, normal_y = vec_x / size, vec_y / size
+
+    return normal_x, normal_y
+    pass
