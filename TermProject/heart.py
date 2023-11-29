6e01088956f15bc6bc9927b002c67235282b24c2
ohtak6843_Laptop <ohtak6843@gmail.com>
2023-11-29 14:04:34 +0900

    생명력 표시 위치 및 사이즈 수정

--- TermProject/heart.py
+++ TermProject/heart.py
@@ -1,12 +1,13 @@
 from pico2d import *
+from define import *
 
 
 class Heart:
     image = None
-    sizeX = 66
-    sizeY = 60
-    x = 66
-    y = 700
+    sizeX = 44
+    sizeY = 40
+    x = 40
+    y = 760
 
     def __init__(self, HP = 6):
         self.HP = HP
@@ -16,7 +17,7 @@ class Heart:
 
     def draw(self):
         for i in range(0, self.HP):
-            Heart.image.draw(self.x + self.sizeX * i, self.y, self.sizeX, self.sizeY)
+            Heart.image.draw(Heart.x + (Heart.sizeX + 10) * i, Heart.y, Heart.sizeX, Heart.sizeY)
 
     def update(self):
         pass
